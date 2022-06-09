5f44b4fa1c8a0547931e800e313dba3008ccc364
ratu <ratukomala28@gmail.com>
2022-06-09 12:41:24 +0700

    menambahkan file codingan

---
+++ footer.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf8" />
+<link href="main.css" rel="stylesheet" type="text/css"></head>
+<body>
+<?php include("variables.php");
+
+     ?>
+ <footer class="footer">
+<div class="container">
+        <h4 class="title" style="text-align:center;margin:0px;text-decoration:underline;">Area Bantuan </h4>
+        <div class="row" style="padding">
+
+        <div style="width:20%;display:inline-block;" class="col">
+            <h4 class="title">Ingin Rumah Tampil Beda & Modern :</h4>
+            <p>Jika sebelumnya anda memiliki rumah dengan desain yang sederhana sehingga membuat anda merasa kurang nyaman dan bosan saat di rumah ataupun anda ingin memiliki rumah yang punya jenis desain rumah yang unik & modern, anda dapat menguhubungi kontak yang sudah kami sediakan di samping. <a href="https://developer.nbg.gr" target="_blank" style="color:white;"></p>
+
+
+            </div>
+        <div style="width:30%;display:inline-block;" class="col">
+            <h4 class="title">Sosial Media:</h4>
+            <p> Anda bisa mengubungi atau mengirim pesan ke kontak atau sosial media yang telah kami sediakan </p>
+            <span class="acount-icon">
+
+            <a> Whatshapp:081987563215</a>
+            <a> Instagram: @Ratu_sukma_komala & @isnaenti</a>
+  <!--           <a>Request-Id: <?php// echo $request_id ; ?></a>
+                 <a>Authorization: <?php// echo $token ; ?></a>
+                 <a>YourVariable: <?php// echo $YourVariable ; ?></a>
+                 <a>YourVariable: <?php// echo $YourVariable ; ?></a>
+                 <a>YourVariable: <?php// echo $YourVariable ; ?></a>
+                 <a>YourVariable: <?php// echo $YourVariable ; ?></a>  -->
+          </span>
+            </div>
+        <div style="width:45%;display:inline-block;" class="col">
+            <h4 class="title">Hastag:</h4>
+            <div class="category">
+            <a href="#">Desain Rumah</a>
+            <a href="#">Rumah modern</a>
+            <a href="#">Rumah berkelas</a>
+            <a href="#">Pembayaran Artsitek</a>
+            <a href="#">Arsitek keren</a>
+            <a href="#">Account Information - Instagram</a>
+            <a href="#">Account Information - Whatshapp</a> 
+            <a href="#">Social Network Platform</a>
+            </div>
+            </div>
+        </div>
+        </div>
+        <hr>
+
+        <div style="text-align:center;margin-top:20px;"> © 2022. Made with &hearts; by Marketplace.RAIS</div>
+
+    </footer>
+</body>
+</html>
